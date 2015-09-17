e2d3d9a1f3f2b3ee7273769e44f708810990e73e
rossoh <rossoliverharrison@gmail.com>
2015-09-17 15:27:09 +0100

    Better layout
    media style foreach video

--- francis/resources/views/videos/index.blade.php
+++ francis/resources/views/videos/index.blade.php
@@ -2,13 +2,30 @@
 
 @section('content')
 
-<h3>Here are all the videos for you to vote on!</h3>
+<div class="container">
 
-	@foreach($videos as $video)
-		{{ $video->studentName }}
-		<a href="/video/show/{{ $video->id }}">View!</a>
-		<img src="/assets/{{ $video->videoThumbnail }}" />
-		<p><i class="glyphicon glyphicon-heart"></i> Upvote!</a></li>
-	@endforeach
+<div class="jumbotron">
+  <h2>Hello, St Bon's!</h2>
+  <p>These videos are all about science, and are all less than a minute long. Watch them, and if you find one you really like, you can click the heart button <i class="glyphicon glyphicon-heart"></i> to give it your vote! The video with the most votes will win an amazing prize! <i class="glyphicon glyphicon-gift"></i></p>
+  <p>If you want to dd your own science video here for everyone to vote on, click here:&nbsp; <a class="btn btn-primary btn-lg" href="/upload" role="button">Add my video!</a></p>
+</div>
+
+<div class="media">
+@foreach($videos as $video)
+  <div class="media-left media-middle">
+    <a href="/video/show/{{ $video->id }}">
+      <img class="media-object" src="/assets/{{ $video->videoThumbnail }}" alt="{{ $video->studentName }}">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">{{ $video->studentName }}</h4>
+    A video by {{ $video->studentName }}!
+	<i class="glyphicon glyphicon-heart"></i> Upvote!
+  </div>
+  <p><br></p>
+</div>
+
+@endforeach
+</div>
 	
 @endsection('content')
