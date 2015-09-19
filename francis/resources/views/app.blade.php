1ef135ae845296227d2b2a307ba3ecc90485d7b4
rossoh <rossoliverharrison@gmail.com>
2015-09-19 12:45:37 +0100

    st bons href

--- francis/resources/views/app.blade.php
+++ francis/resources/views/app.blade.php
@@ -32,7 +32,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand"  href="#">St Bonaventure's</a>
+      <a class="navbar-brand"  href="/">St Bonaventure's</a>
     </div>
 
     <!-- Nav links, forms, and other content for toggling -->
