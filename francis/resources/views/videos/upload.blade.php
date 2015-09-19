6c0ed9355b2063364854895fdb3fdfae2e1fd113
immediatemedia-dan <daniel.tribe@immediate.co.uk>
2015-09-19 15:13:40 +0100

    Ross' new form work

--- francis/resources/views/videos/upload.blade.php
+++ francis/resources/views/videos/upload.blade.php
@@ -7,27 +7,27 @@
 {!! Form::open(array('url' => '/video/upload', 'files' => true)) !!}
 
 			<div class="form-group">
-				{!! Form::label('studentName', 'Name:') !!}
+				<span class="glyphicon glyphicon-user"></span>&nbsp;{!! Form::label('studentName', 'What is your full name?') !!}
 				{!! Form::text('studentName', "", ['class' =>'form-control']) !!}
 			</div>
 			
 			<div class="form-group">
-				{!! Form::label('className', 'Class name:') !!}
-				{!! Form::text('className', "", ['class' =>'form-control']) !!}
+				<span class="glyphicon glyphicon-education"></span>&nbsp;{!! Form::label('className', 'Which class are you in?:') !!}
+				{!! Form::select('className', array('RM', 'RH', '1H', '1MS', '2T', '2WR', '3J', '3T', '4SA', '4C', '5P', '5K', '6D', '6GW'), ['class' =>'form-control']) !!}
 			</div>
 			
 			<div class="form-group">
-				{!! Form::label('houseName', 'House name:') !!}
+				<span class="glyphicon glyphicon-home"></span>&nbsp;{!! Form::label('houseName', 'Which house are you in?:') !!}
 				{!! Form::text('houseName', "", ['class' =>'form-control']) !!}
 			</div>
 			
 			<div class="form-group">
-				{!! Form::label('video', 'Your video:') !!}
+				<span class="glyphicon glyphicon-film"></span>&nbsp;{!! Form::label('video', 'Find where you saved your video:') !!}
 				{!! Form::file('video') !!}
 			</div>
 			
 			<div class="form-group">
-				{!! Form::submit('Save Changes') !!}
+				<span class="glyphicon glyphicon-plus"></span>&nbsp;{!! Form::submit('Add your video!') !!}
 			</div>
 
 {!! Form::close() !!}
