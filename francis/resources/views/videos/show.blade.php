60f9b40c533e6cc00729d71b12a2feee4a919e12
rossoh <rossoliverharrison@gmail.com>
2015-09-20 11:41:56 +0100

    Video page

--- francis/resources/views/videos/show.blade.php
+++ francis/resources/views/videos/show.blade.php
@@ -12,18 +12,23 @@
 	 <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 	</video>
 	
-	<!-- Stack the columns on mobile by making one full-width and the other half-width -->
-	<div class="row">
-		<div class="col-xs-12 col-md-9"><h3>{{ $video->title }} &nbsp; <i>{{ $video->studentName }}</i></h3></div>
-		<div class="col-xs-6 col-md-3"><h3 class="text-muted">{{ $video->houseName }}, {{ $video->className }}</h4></div>
-	</div>
-	<div class="row">
-		<div class="col-xs-12 col-md-12"><h4> {{ $video->videoDescription }} </h4></div>
-
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<div class="row">
+				<div class="col-xs-12 col-md-9"><h3 class="bg-primary" style="margin-top: 0px;">{{ $video->title }} &nbsp; <i>{{ $video->studentName }}</i></h3></div>
+				<div class="col-xs-6 col-md-3"><h3>{{ $video->houseName }}, {{ $video->className }}</h3></div>
+			</div>
+		</div>
+		<div class="panel-body">
+			<div class="row">
+				<div class="col-xs-12 col-md-12"><h4> {{ $video->videoDescription }} </h4>
+			</div>
+		</div>
+	
 	<h3 id="rating">{{ $video->videoRating }} {{ $video->voteSuffix }}</h3>
 	<h4 class="text-primary">&nbsp;If you really like this video, click the heart to give it your vote! &nbsp; <a onclick="upvote();" class="glyphicon glyphicon-heart"></a></h4>
 	<div id="videoRating">
-	
+</div>
 @endsection('content')
 </div>
 
