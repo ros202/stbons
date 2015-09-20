75bca8ca62fbc5b525330f08b2c36c1bb5f79a8a
rossoh <rossoliverharrison@gmail.com>
2015-09-20 22:46:52 +0100

    added register fields

--- francis/resources/views/videos/register.blade.php
+++ francis/resources/views/videos/register.blade.php
@@ -10,6 +10,21 @@
 	</div>
 	
 	<div class="form-group">
+		<span class="text-primary"><span class="glyphicon glyphicon-envelope"></span></span>&nbsp;{!! Form::label('email', 'Provide an email address:') !!}
+		{!! Form::text('email', "", ['class' =>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+		<span class="text-primary"><span class="glyphicon glyphicon-lock"></span></span>&nbsp;{!! Form::label('password', 'Create a strong password:') !!}
+		{!! Form::text('password', "", ['class' =>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+		<span class="text-primary"><span class="glyphicon glyphicon-check"></span></span>&nbsp;{!! Form::label('confirmPassword', 'Confirm your password:') !!}
+		{!! Form::text('confirmPassword', "", ['class' =>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
 		<span class="text-primary"><span class="glyphicon glyphicon-plus"></span></span>&nbsp;{!! Form::submit('Create new user') !!}
 	</div>
 
