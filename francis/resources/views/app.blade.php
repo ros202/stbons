1f4a6fe8b4d5acd34291f42f9089b856bffca3a7
rossoh <rossoliverharrison@gmail.com>
2015-09-20 12:33:30 +0100

    Responsive video
    changed video frame to responsive to fix iPhone viewport problem. Does
    any of the commented-out stuff need to be added to the new video div?
    
    (also meta names in the app blade)

--- francis/resources/views/app.blade.php
+++ francis/resources/views/app.blade.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="description" content="">
-<meta name="author" content="Scotch">
+<meta name="description" content="St Bonaventure's voting site">
+<meta name="author" content="Ross Harrison">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="logo.png" />
 
