04d9e0aa8af091f911329b09b1a10b3d2a96b50f
immediatemedia-dan <daniel.tribe@immediate.co.uk>
2015-10-07 21:33:21 +0100

    DT

--- francis/app/Http/Controllers/VideosController.php
+++ francis/app/Http/Controllers/VideosController.php
@@ -148,14 +148,15 @@ class VideosController extends Controller
 	
 	// Upvote video
 	public function upvote($id) {
+		$video = Videos::where('id', '=', $id)->first();
+		$video->voteSuffix = ($video->videoRating != 1 ? "votes": "vote");
+						
 		if(null !== Session::get('user.votes')) {
 			if(count(Session::get('user.votes')) < 2) {
 			
 				foreach(Session::get('user.votes') as $existingVote) {
 					if($existingVote == $id) {
 						// Video previously voted for
-						$video = Videos::where('id', '=', $id)->first();
-						$video->voteSuffix = ($video->videoRating != 1 ? "votes": "vote");
 						return $video->videoRating . " " . $video->voteSuffix . " &ndash; You've already voted!";
 					}
 				}
